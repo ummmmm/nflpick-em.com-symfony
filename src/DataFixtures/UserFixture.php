6e247d1c79fd47bc86fafcf44d1240dccb29eb0f
ummmmm <carve006@cougars.csusm.edu>
2021-01-28 13:44:14 -0800

    Set the default user to have a hash password

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -22,13 +22,14 @@ class UserFixture extends Fixture
     	$user->setFirstname( 'Bob' );
     	$user->setLastname( 'Barker' );
     	$user->setEmail( 'bbarker@example.com' );
-    	$user->setPassword( $this->encoder->encodePassword( $user, 'P@ssw0rd' ) );
+    	$user->setPassword( '$2y$13$M9jIMvn42iZndTx0TUsHje9oEnBKAQUcXG6zgFDjEsiRt9vwzg/UC' );
     	$user->setWins( 0 );
     	$user->setLosses( 0 );
     	$user->setPaid( true );
     	$user->setCurrentPlace( 1 );
     	$user->setActive( true );
     	$user->setMessage( '' );
+    	$user->setRoles( [ 'ROLE_ADMIN' ] );
 
     	$manager->persist( $user );
         $manager->flush();
